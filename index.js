1ca05c75d1b7ff9adb87a3be6990bd3cf2a67739
ftsxyghu <js1girgirem@hotmail.com>
2024-05-09 14:07:13 +0700

    aptos

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-function isAnagram(s, t) {
-  if (s.length !== t.length) return false;
-  const count = Array(26).fill(0);
-  for (let i = 0; i < s.length; i++) {
-    count[s.charCodeAt(i) - 97]++;
-    count[t.charCodeAt(i) - 97]--;
-  }
-  return count.every((c) => c === 0);
+function factorial(n) {
+  if (n === 0 || n === 1) return 1;
+  return n * factorial(n - 1);
 }
