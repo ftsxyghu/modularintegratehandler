686785ccdd2486bbe5b9afab3e0af312bb156d36
ftsxyghu <js1girgirem@hotmail.com>
2024-05-13 09:14:30 +0700

    feat project

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-function wiggleMaxLength(nums) {
-  if (nums.length === 0) return 0;
-  let up = 1;
-  let down = 1;
-  for (let i = 1; i < nums.length; i++) {
-    if (nums[i] > nums[i - 1]) {
-      up = down + 1;
-    } else if (nums[i] < nums[i - 1]) {
-      down = up + 1;
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let key = arr[i];
+    let j = i - 1;
+    while (j >= 0 && arr[j] > key) {
+      arr[j + 1] = arr[j];
+      j--;
     }
+    arr[j + 1] = key;
   }
-  return Math.max(up, down);
+  return arr;
 }
